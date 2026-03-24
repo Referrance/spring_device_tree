f4925d36cf66d66158affd2d7d56bdbfc967676c
PhantomTimed <mlebmleb843@gmail.com>
2026-03-24 21:30:58 +1300

    New version
    fixed a lot of the problems we had before

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_spring.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_spring-user \
+    lineage_spring-userdebug \
+    lineage_spring-eng
